c8b74a91ffb6b152b2fc54b873464698ffeb2f55
jamerrq <jamerrq@gmail.com>
2024-03-05 20:29:22 -0500

    feat: tranquiluxe bg from uvcanvas

---
+++ src/components/global/Background/Tranquiluxe.tsx
@@ -0,0 +1,9 @@
+import { Tranquiluxe } from "uvcanvas"
+
+export default function Background() {
+    return (
+        <div className="w-[100dvw] h-[100dvh] -z-10 absolute">
+            <Tranquiluxe />
+        </div>
+    )
+}
