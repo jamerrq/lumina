3e12d6935fdde99461a8bd62efc0782a1d599f7d
jamerrq <jamerrq@gmail.com>
2024-03-05 19:58:09 -0500

    feat: styles & enhancement

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
-# Lumina - PWA Starter Template
+# LUMINA
+
+![alt text](./lib/snaps/preview.png)
+
+## About
+
+Template for a PWA with Astro and TailwindCSS. Prettier, EsLint and VsCode settings included.
 
 Generated from [`astro-pwa-starter`](https://github.com/shaunchander/astro-pwa-starter)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,24 +2,24 @@
 export default {
   content: ['./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}'],
   theme: {
-    extend: {
-      screens: {
-        sm: '640px',
-        // => @media (min-width: 640px) { ... }
+    screens: {
+      xs: '320px',
+      // => @media (min-width: 320px) { ... }
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
 
-        md: '768px',
-        // => @media (min-width: 768px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
 
-        lg: '1024px',
-        // => @media (min-width: 1024px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
 
-        xl: '1280px',
-        // => @media (min-width: 1280px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
 
-        '2xl': '1536px'
-        // => @media (min-width: 1536px) { ... }
-      }
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
     }
   },
-  plugins: ['tailwindcss-debug-screens']
+  plugins: [require('tailwindcss-debug-screens'), 'prettier-plugin-tailwindcss']
 }
